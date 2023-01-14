9239383f775f01965d42e988e714b96d2d8a1a7b
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2023-01-14 20:03:47 +0100

    Add files via upload

---
+++ Zbozi/Zbozi/Produkt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbozi
+{
+    internal class Produkt:Zbozi
+    {
+        bool lzezlevnit;
+        public Produkt(string nazev, int cena, DateTime datumvyroby, int minimalnitrvanlivost,bool lzezlevnit) : base(nazev, cena, datumvyroby, minimalnitrvanlivost)
+        { 
+            this.lzezlevnit= lzezlevnit;
+        }
+
+        public override double cenasDPH()
+        {
+            if(Splnujemintrv())
+            {
+                return (cena * 1.21);
+            }
+            else
+            {
+                return (cena * 1.21) * (0.60);
+            }
+        }
+
+    }
+}
